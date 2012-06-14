21d584c90eaca448fe1a0fb11c09806b144865e0
devaux.br@gmail.com <devaux.br@gmail.com@fb244b1f-71f8-cfce-7d5c-0ee98cb357b8>
2012-06-14 08:01:27 +0000

    Et les flights continuent leur petit bout de chemin : controller complété, models créés (mais pas complété...), etc.

--- trunk/controller/flights.controller.php
+++ trunk/controller/flights.controller.php
@@ -1,4 +1,24 @@
 <?php
 
+
 require DIR_MODEL.'flights.model.php';
+
+if($getSection == 'new')
+{
+	require DIR_MODEL.'flights.new.model.php';
+}
+elseif($getSection == 'create')
+{
+	require DIR_MODEL.'flights.create.model.php';
+}
+elseif($getSection == 'search')
+{
+	require DIR_MODEL.'flights.search.model.php';
+}
+else if($getSection == 'del')
+{
+	require DIR_MODEL.'flights.del.model.php';
+	require DIR_MODEL.'flights.search.model.php';
+}
+
 require DIR_VIEW.'flights.view.php';
