02d6dc71b5eeffb8d3e7801f5aba8fe7523b8bb6
trk.yassine@gmail.com <trk.yassine@gmail.com@fb244b1f-71f8-cfce-7d5c-0ee98cb357b8>
2012-06-15 15:48:18 +0000

    quelques fichiers ^^

---
+++ trunk/model/fleet.newModele.model.php
@@ -0,0 +1,48 @@
+<?php
+
+if(isset($_POST, $_POST['newNomModele'], $_POST['newCapacite'], $_POST['newFret']))
+{
+	$nom = $_POST['newNomModele'];
+	$nom = $nom != '' ? $nom : NULL; // On met à NULL si la chaîne est vide
+	// On récupère les autres variables
+	$capacite = $_POST['newCapacite'];
+	$fret = $_POST['newFret'];
+
+	if(isset($nom))
+	{
+		try 
+		{
+			//Commencer une transaction
+			$bdd->beginTransaction();
+			
+			$newVille = $bdd->prepare("INSERT INTO modele(id, nom, capacite_fret, capacite_voyageur) VALUES (nextval('modele_id_seq'), :nom, :fret, :capacite);");
+			$r = $newVille->execute(array(":nom" => $nom, "fret" ==> $fret, ":capacite" ==> $capacite));
+			
+			if($r)
+				$commit = true;
+			else
+				$commit = false;
+			
+			if($commit) // 1 ligne a été ajoutée
+			{				
+				$bdd->commit();
+				$success[] = "Nouveau modèle (".$nom.") ajoutée avec succès.";
+				}
+			else
+			{
+				$bdd->rollback();
+				$errors[] = "Echec lors de l'ajout du modèle";
+			}
+		} 
+		catch (PDOException $e)  //Gestion des erreurs causées par les requêtes PDO
+		{
+			//Annuler la transaction
+			if($bdd) $bdd->rollBack();
+			
+			//Afficher l'erreur
+			$errors[] = "Échec : " . $e->getMessage();
+		}
+	}
+}
+else
+	$infos[] = "Aucune information reçue. Aucun modèle ajouté.";
