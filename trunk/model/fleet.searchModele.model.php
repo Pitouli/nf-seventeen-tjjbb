a9a0c43a82e0146fb2bdbd8a2833f6edaaff147d
trk.yassine@gmail.com <trk.yassine@gmail.com@fb244b1f-71f8-cfce-7d5c-0ee98cb357b8>
2012-06-18 17:53:32 +0000

    Test correction recherche de modèles

--- trunk/model/fleet.searchModele.model.php
+++ trunk/model/fleet.searchModele.model.php
@@ -5,18 +5,16 @@ if(isset($_POST))
 	// On rajoute les % pour indiquer que le motif n'est ni nécessairement en début, ni nécessairement en fin de chaîne
 	$nom = isset($_POST['searchNomModele']) ? '%'.trim($_POST['searchNomModele']).'%' : NULL;
 	$capaciteMin = (!empty($_POST['searchCapaciteMin'])) ? $_POST['searchCapaciteMin'] : 0;	// Au cas où l'utilisateur n'entre rien, afin de prendre tous les résultats.
-	$capaciteMax = (!empty($_POST['searchCapaciteMax'])) ? $_POST['searchCapaciteMax'] : 1000000;
+	$capaciteMax = (!empty($_POST['searchCapaciteMax'])) ? $_POST['searchCapaciteMax'] : 10000;
 	$fretMin = (!empty($_POST['searchFretMin'])) ? $_POST['searchFretMin'] : 0;
-	$fretMax = (!empty($_POST['searchFretMax'])) ? $_POST['searchFretMax'] : 1000000;
+	$fretMax = (!empty($_POST['searchFretMax'])) ? $_POST['searchFretMax'] : 10000;
 	
-	
-	$resultats= array();
 
 	$selectResultats = $bdd->prepare("SELECT id, nom, capacite_fret, capacite_voyageur
 									FROM modele WHERE UPPER(nom) LIKE UPPER(:nom)
 									AND capacite_fret BETWEEN :fretMin AND :fretMax
 									AND capacite_voyageur BETWEEN :capaciteMin AND :capaciteMax
-									AND  LIMIT 100");
+									LIMIT 100");
 									
 	$selectResultats->execute(array(":nom" => $nom, ":fretMin" => $fretMin, ":fretMax" => $fretMax, ":capaciteMin" => $capaciteMin, ":capaciteMax" => $capaciteMax));
 	$resultats = $selectResultats->fetchAll();
